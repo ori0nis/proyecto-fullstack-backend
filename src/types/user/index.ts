cb8582a453e49af39b11227a1669d6030dd99c2d
ori0nis <oihanegdcl@gmail.com>
2025-11-08 00:04:03 +0100

    fix: fixed inconsistent login and getCurrentUser responses

--- src/types/user/index.ts
+++ src/types/user/index.ts
@@ -1,7 +1,6 @@
 export * from "./User.type.js";
 export * from "./UserResponse.type.js";
 export * from "./LoginUser.type.js";
-export * from "./SingleUserResponse.type.js";
 export * from "./PublicUser.type.js";
 export * from "./NewUser.type.js";
 export * from "./UserProfile.js";
